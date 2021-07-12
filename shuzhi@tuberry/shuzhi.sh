f7ce83c4df0c6171071de9fc1dad1c2a031e1bc4
mechtifs <mechtifs@gmail.com>
2021-07-12 13:36:21 +0800

    Implement mingju API instead of parsing json thus jq is no longer needed

--- shuzhi@tuberry/shuzhi.sh
+++ shuzhi@tuberry/shuzhi.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-# by tuberry & mechtifs, need jq installed
+# by tuberry & mechtifs
 
-mj=~/.local/share/gnome-shell/extensions/shuzhi@tuberry/mingju.json
-rand=$(shuf -i 0-9999 -n 1)
-shu=($(jq -r ".[$rand] | .contents + \" \" + .source" "$mj"))
-ori=($(dconf read /org/gnome/shell/extensions/shuzhi/text-orientation))
-if [[ ${ori[1]} -eq "0" ]]
+#API from https://github.com/xuchunyang/mingju
+api='https://mingju.vercel.app/api/random'
+res=$(curl -s $api)
+while [[ "$res" == *"error"* ]]
+do
+	res=$(curl -s $api)
+done
+contents=$(echo $res | grep -Po '(?<="contents": ")[^"]*')
+source=$(echo $res | grep -Po '(?<="source": ")[^"]*')
+
+#Detect text orientaion
+orien=($(dconf read /org/gnome/shell/extensions/shuzhi/text-orientation))
+
+#Text processing
+if [[ ${orien[1]} -eq '0' ]]
 then
-	body=$(echo ${shu[0]} | sed -r '/《/d;/》/d;')
-	head=($(echo ${shu[1]} | sed -e 's/《/\n「/g;s/》/」/g'))
+	body=$(echo $contents | sed -r '/《/d;/》/d;')
+	head=($(echo $source | sed -e 's/《/\n「/g;s/》/」/g'))
 else
-	body=$(echo ${shu[0]} | sed -r 's/[，。：；？、！]/\n/g;/《/d;/》/d;' | sed -r '/^\s*$/d')
-	head=($(echo ${shu[1]} | sed -e 's/《/\n「/g;s/》/」/g'))
+	body=$(echo $contents | sed -r 's/[，。：；？、！]/\n/g;/《/d;/》/d;' | sed -r '/^\s*$/d')
+	head=($(echo $source | sed -e 's/《/\n「/g;s/》/」/g'))
 fi
 printf '%s\n' $body '<span font="16"> </span>' '<span font="18">'${head[1]}'<span bgcolor="#9a2e36" fgcolor="#eee">'${head[0]}'</span></span>'
